5a3fa78dc1d66cf7c741a16334979554b5b66331
SergeyKharris <Scar2000@yandex.ru>
2018-10-19 16:27:04 +0300

    some emails was added
    1. black friday
    2. rus version in progress

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) { 
+module.exports = function(grunt) {
 	grunt.initConfig({
 	    inlinecss: {
 	        main: {
@@ -11,7 +11,9 @@ module.exports = function(grunt) {
 	                'email_inlined_auto.html': 'email.html',
 	                'email2_inlined_auto.html': 'email2.html',
 	                'email_africa1_inlined_auto.html': 'email_africa1.html',
-	                'email3_en_inlined_auto.html': 'email3_en.html'
+	                'email3_en_inlined_auto.html': 'email3_en.html',
+									'emai_bf_inlined.html': 'email_black_friday.html',
+									'email4_inlined.html': 'email4_rus.html'
 	            },
 
 	        }
