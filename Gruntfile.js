5ac1fb6bb4392d1c9e5a961e6a46e6983b142083
SergeyKharris <Scar2000@yandex.ru>
2018-11-06 15:59:55 +0300

    some fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function(grunt) {
 	                'email2_inlined_auto.html': 'email2.html',
 	                'email_africa1_inlined_auto.html': 'email_africa1.html',
 	                'email3_en_inlined_auto.html': 'email3_en.html',
+									'email4_inlined.html': 'email3_en.html',
 									'emai_bf_inlined.html': 'email_black_friday.html',
 									'email_bf_rus.html': 'email_black_friday_rus.html'
 	            },
